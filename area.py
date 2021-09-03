f1aac220a0f03ec6b2306a30a6e53ea0cfa1a3ba
d4nnr <danielromogarcia@gmail.com>
2021-09-03 17:18:38 -0500

    Update area.py

--- area.py
+++ area.py
@@ -4,56 +4,60 @@
 from sys import argv
 import math
 
-fig = int(input("""
-                    1: Cuadrado
-                    2: Rectangulo
-                    3: Triangulo
-                    4: Circulo
+try:
+    fig = int(input("""
+                        1: Cuadrado
+                        2: Rectangulo
+                        3: Triangulo
+                        4: Circulo
 
-                    Selecciona la figura: """))
+                        Selecciona la figura: """))
 
-if fig == 1:
-    print('Elegiste el Cuadrado')
-    print("\n")
-    print('...Vamos a calcular el area :)')
-    print("\n")
-    l1 = int(input("ingresa el valor del lado: "))
-    ok = l1 ** 2
-    print("\n")
-    print("Bien, el área es", ok)
-    print("\n")
+    if fig == 1:
+        print('Elegiste el Cuadrado')
+        print("\n")
+        print('...Vamos a calcular el area :)')
+        print("\n")
+        l1 = int(input("ingresa el valor del lado: "))
+        ok = l1 ** 2
+        print("\n")
+        print("Bien, el área es", ok)
+        print("\n")
 
-elif fig == 2: 
-    print('Elegiste el  Rectangulo') 
-    print("\n")
-    print('...Vamos a calcular el area :)')
-    print("\n")
-    b = int(input("ingresa el valor de la base: "))
-    h = int(input("Ahora ingresa el valor de la altura: "))
-    ok = b*h
-    print("\n")
-    print("Bien, el área es", ok)
-    print("\n")
+    elif fig == 2: 
+        print('Elegiste el  Rectangulo') 
+        print("\n")
+        print('...Vamos a calcular el area :)')
+        print("\n")
+        b = int(input("ingresa el valor de la base: "))
+        h = int(input("Ahora ingresa el valor de la altura: "))
+        ok = b*h
+        print("\n")
+        print("Bien, el área es", ok)
+        print("\n")
 
-elif fig == 3: 
-    print('Elegiste el Triangulo')
-    print("\n")
-    print('...Vamos a calcular el area :)')
-    print("\n")
-    b = int(input("ingresa el valor la base: "))
-    h = int(input("Ahora ingresa el valor de la altura: "))
-    ok = b*h/2
-    print("\n")
-    print("Bien, el área es", ok)
-    print("\n")
+    elif fig == 3: 
+        print('Elegiste el Triangulo')
+        print("\n")
+        print('...Vamos a calcular el area :)')
+        print("\n")
+        b = int(input("ingresa el valor la base: "))
+        h = int(input("Ahora ingresa el valor de la altura: "))
+        ok = b*h/2
+        print("\n")
+        print("Bien, el área del cuadrado es", ok)
+        print("\n")
 
-elif fig == 4:   
-    print('Elegiste el Circulo')
-    print("\n")
-    print('...Vamos a calcular el area :)')
-    print("\n")
-    R = int(input("ingresa el valor del radio: "))
-    ok = math.pi*R**2
-    print("\n")
-    print("Bien, el área es", ok)
-    print("\n")
+    elif fig == 4:   
+        print('Elegiste el Circulo')
+        print("\n")
+        print('...Vamos a calcular el area :)')
+        print("\n")
+        R = int(input("ingresa el valor del radio: "))
+        ok = math.pi*R**2
+        print("\n")
+        print("Bien, el área del cuadrado es", ok)
+        print("\n")
+
+except ValueError:
+    print ('Recuerden ingresar solo numeros')
